0e0a740bc235b8d85ed147dfced7eaf728b32fed
amewi <amewi@DESKTOP-FDA8HPC>
2021-05-27 09:40:27 -0400

    initial commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,30 @@
+package studentlist;
+
+/*
+ * Jinyoung Jeon
+ * Student ID: 991622283
+Students in our application
+ */
+
+/**
+ *
+ * @author amewi
+ */
+public class Student {
+    
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
